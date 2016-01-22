605afe110211d9b5687ee4ee9ecb1e87bbcec087
DdMad <ddmad1030@gmail.com>
2016-01-22 15:48:59 +0800

    Auto fill the input with formatted address.

--- www/js/controllers/routeController.js
+++ www/js/controllers/routeController.js
@@ -46,34 +46,33 @@ angular.module('MyApp')
 
       var autocomplete = new google.maps.places.Autocomplete(input, options);
       autocomplete.bindTo('bounds', map);
-      autocomplete.setTypes([]);
+      autocomplete.setTypes(['geocode']);
 
-      var infowindow = new google.maps.InfoWindow();
+      // var infowindow = new google.maps.InfoWindow();
       var marker = new google.maps.Marker({
         map: map,
         anchorPoint: new google.maps.Point(0, -29)
       });
 
-      autocomplete.addListener('place_changed', function() {
-        infowindow.close();
+      autocomplete.addListener('place_changed', function(event) {
+        // infowindow.close();
         marker.setVisible(false);
 
         // Get autocomplete address
         var place = autocomplete.getPlace();
-        console.log(place);
+
+        console.log(place.formatted_address);
 
         if (!place.geometry) {
-          console.log(
-            'Autocomplete\'s returned place contains no geometry');
+          console.log('Autocomplete\'s returned place contains no geometry');
           return;
         }
 
-        // If the place has a geometry, then present it on a map.
         if (place.geometry.viewport) {
           map.fitBounds(place.geometry.viewport);
         } else {
           map.setCenter(place.geometry.location);
-          map.setZoom(17); // Why 17? Because it looks good.
+          map.setZoom(17);
         }
         marker.setIcon({
           url: place.icon,
@@ -85,19 +84,16 @@ angular.module('MyApp')
         marker.setPosition(place.geometry.location);
         marker.setVisible(true);
 
-        var address = '';
-        if (place.address_components) {
-          address = [
-            (place.address_components[0] && place.address_components[0]
-              .short_name || ''), (place.address_components[1] && place
-              .address_components[1].short_name || ''), (place.address_components[
-              2] && place.address_components[2].short_name || '')
-          ].join(' ');
-        }
-
-        infowindow.setContent('<div><strong>' + place.name +
-          '</strong><br>' + address);
-        infowindow.open(map, marker);
+        // Set the input to formatted address manually
+        setTimeout(function() {
+          if (id == 'from-address') {
+            $scope.deliveryInfo.from = place.formatted_address;
+            document.getElementById(id).focus();
+          } else if (id == 'to-address') {
+            $scope.deliveryInfo.to = place.formatted_address;
+            document.getElementById(id).focus();
+          }
+        }, 0);
       });
     }
 
